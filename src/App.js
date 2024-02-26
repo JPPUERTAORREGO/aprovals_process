132e4ab7c95389c656d77fdc3b38dcb58eb60552
ChicaJupiter <julianacar2901@gmail.com>
2024-02-26 16:32:03 -0500

    Version 1
    Diseño de navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import { Header_component } from './components/Header_component';
+import  Header_component  from './components/Header_component';
 import { List_component } from './components/List_component';
 import { Detail_component } from './components/Detail_component';
 
--- src/components/Header_component.js
+++ src/components/Header_component.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import './Header_component.css'
 
 export const Header_component = () => {
   return (
-    <div>
-      <h1>este es Header</h1>
+    <div class="contenedor_navbar">
+      <nav class="navbar">
+        <div>
+          <img class="logop" src="user.png" alt="logo"/>
+        </div>
+        <div class="tipoCuenta">
+          <h3>Administrador</h3>
+        </div>
+        <div class="container">
+          <div>
+          </div>
+          <h2>Flujo de aprovaciones</h2>
+            <ul class="nav-links">
+                <li><a href="#"><img class="imagenes" src="inicio.png" alt="user" width="30" height="30"/></a></li>
+                <li><a href="#"><img class="imagenes" src="user.png" alt="user" width="30" height="30"/></a></li>
+                <li><a href="#"><img class="imagenes" src="agregar.png" alt="agregar" width="30" height="30"/></a></li>
+                <li><a href="#"><img class="imagenes" src="salir.png" alt="salir" width="30" height="30"/></a></li>
+            </ul>
+        </div>
+      </nav>
     </div>
   )
 }
+
+export default Header_component;
