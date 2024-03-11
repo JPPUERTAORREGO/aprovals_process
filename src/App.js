3a9cde9a723c3b477a51e46b48fdaaa88f942cc9
ChicaJupiter <julianacar2901@gmail.com>
2024-03-11 15:05:23 -0500

    version 1
    Diseño del navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { Prueba } from './components/Prueba';
+import { Header_component } from './components/Header_component';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header_component/>
       <Prueba/>
     </div>
   );
--- src/components/Header_component.js
+++ src/components/Header_component.js
@@ -1,12 +1,35 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header_component.css' // Importamos los estilos CSS para este componente.
 
 export const Header_component = (props) => {
 
   return (
     <div>
-
+      <Navbar collapseOnSelect expand="lg" className="bg bg-dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src="img/user.png" alt="user" width="30" height="30"/>
+            <h5>Administrador</h5>
+          </Navbar.Brand>
+          
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <div className="navbar-center">
+              <h2>Flujo de Aprobaciones</h2>
+            </div>
+            <div>
+              <a href="#"><img className="imagenes" src="img/inicio.png" alt="user" width="30" height="30"/></a>
+              <a href="#"><img className="imagenes" src="img/user.png" alt="user" width="30" height="30"/></a>
+              <a href="#"><img className="imagenes" src="img/agregar.png" alt="agregar" width="30" height="30"/></a>
+              <a href="#"><img className="imagenes" src="img/salir.png" alt="user" width="30" height="30"/></a>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   )
 }
