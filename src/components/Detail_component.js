35ebaaf52761095b3491b86c35d93fd3e44f7191
KEV-8A <133246538+KEV-8A@users.noreply.github.com>
2024-03-11 10:03:14 -0500

    ultma version antes de hacer commit en master

--- src/components/Detail_component.js
+++ src/components/Detail_component.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import Data from '../Data.js';
 import './Detail.css';
 import Accordion from 'react-bootstrap/Accordion';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export const Detail_component = ({ cambiarPosicion, cambiarContador, posicion, contador, activeKey, setActiveKey }) => {
   const usuario = Data.solicitudes[posicion];
@@ -35,57 +37,70 @@ export const Detail_component = ({ cambiarPosicion, cambiarContador, posicion, c
 
   return (
     <>
-    <Accordion activeKey={activeKey}>
-      <Accordion.Item eventKey={String(posicion)}>
-        <Accordion.Header onClick={handleAccordionToggle}>{usuario['tipo de novedad'][usuario.id]} </Accordion.Header>
-        <Accordion.Body>
-          <section className='set'>
-            <h2 className='titulo'>Detail</h2>
-            <div className='principal'>
-              <div className='princi'>
+      <Accordion activeKey={activeKey}>
+        <Accordion.Item eventKey={String(posicion)}>
+          <Accordion.Header onClick={handleAccordionToggle}>{usuario['tipo de novedad'][usuario.id]} </Accordion.Header>
+          <Accordion.Body>
+            <section className='set'>
+              <h2 className='titulo'>Detail</h2>
+              <div className='principal'>
                 <div key={usuario.id} className='usuario-info'>
-                  <h2>{usuario['tipo de novedad'][usuario.id]} {usuario.Descripción}</h2>
-                  <p>codigo de equipo: {contador}</p>
-                  <p>Ubicacion técnica: {usuario['Ubicacion técnica']}</p>
-                  <p>Justificación: {usuario.Justificación}</p>
-                  <p>Valor activo: {usuario['Valor activo']}</p>
-                  <p>Archivo: {usuario.Archivo ? 'Activo' : 'Inactivo'}</p>
-                  <p>Aprovador: {usuario.Aprovador[usuario.id]}</p>
-                  <div className='bttn-increment'>
-                    <button onClick={() => { cambiarContador(contador + 1) }}>incrementa</button>
-                    <button onClick={() => { cambiarContador(contador - 1) }}>decrementa</button>
-                  </div>
+                  <Card style={{ width: '40rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                    <h2>{usuario['tipo de novedad'][usuario.id]} {usuario.Descripción}</h2>
+                      <Card.Title>usuario: {[usuario.id]} </Card.Title>
+                      <Card.Text>
+                        <p>codigo de equipo: {contador}</p>
+                        <p>Ubicacion técnica: {usuario['Ubicacion técnica']}</p>
+                        <p>Justificación: {usuario.Justificación}</p>
+                        <p>Valor activo: {usuario['Valor activo']}</p>
+                        <p>Archivo: {usuario.Archivo ? 'Activo' : 'Inactivo'}</p>
+                        <p>Aprovador: {usuario.Aprovador[usuario.id]}</p>
+                      </Card.Text>
+                      <Button className='bttn-increment' onClick={() => { cambiarContador(contador + 1) }} variant="primary">incrementa</Button>
+                      <Button onClick={() => { cambiarContador(contador - 1) }} variant="primary">decrementa</Button>
+                    </Card.Body>
+                  </Card>
                 </div>
               </div>
-            </div>
-            <div className='historial'>
-              <h3>Historial de comentarios</h3>
-              <div className='histo1'>
-                <input
-                  className='histo2'
-                  placeholder='Comentar'
-                  type='text'
-                  value={comentario}
-                  onChange={handleComentarioChange}
-                />
+              <div className='historial'>
+                <Card style={{ width: '35rem'}}>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                  <Card.Body>
+                    <Card.Title></Card.Title>
+                    <Card.Text>
+                      <h3>Historial de comentarios</h3>
+                      <div className='histo1'>
+                        <input 
+                          className='histo2'
+                          placeholder='Comentar'
+                          type='text'
+                          value={comentario}
+                          onChange={handleComentarioChange}
+                        />
+                      </div>
+                       <Button className='envia' onClick={handleComentarioSubmit} variant="primary">Enviar</Button>
+                      {historialComentarios.map((comentario, index) => (
+                        <div key={index} className='histo1'>
+                          <p>{comentario.comentario}</p>
+                          <p>{comentario.fecha}</p>
+                        </div>
+                      ))}
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+
               </div>
-              <button className='envia' onClick={handleComentarioSubmit}>Enviar</button>
-              {historialComentarios.map((comentario, index) => (
-                <div key={index} className='histo1'>
-                  <p>{comentario.comentario}</p>
-                  <p>{comentario.fecha}</p>
-                </div>
-              ))}
-            </div>
-            <div className='solicitud'>
-              <h2>Solicitud 1</h2>
-              <img src='icons8-emoji-circulo-verde-48.png' alt='Emoji verde' />
-            </div>
-          </section>
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-  </>
+              <div className='solicitud'>
+                <h2>Solicitud {[usuario.id]}</h2>
+                <img src='icons8-emoji-circulo-verde-48.png' alt='Emoji verde' />
+              </div>
+            </section>
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </>
   );
 };
 
