562135db3ff5922e0b1367ddaa6cf9d057f07e6a
ChicaJupiter <julianacar2901@gmail.com>
2024-03-14 15:26:29 -0500

    ....

--- src/components/List_component.js
+++ src/components/List_component.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const List_component = () => {
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className="listComponent">
       {Data.solicitudes.map((solicitud, index) => (  // itera sobre los datos
         <Card key={index} className="cardComponent" style={{ width: '18rem' }}>
@@ -43,6 +44,13 @@ export const List_component = () => {
 
 
 export default List_component;
+=======
+    <div>
+      <h1>este es Center</h1>
+    </div>
+  )
+}
+>>>>>>> parent of 34b4e87 (Version 8)
 =======
     <div>
       <h1>este es Center</h1>
